fbc300bab88b9d5ab8594f3efa4cd52e5a3b10d6
CarlosAguayo1 <carlosibanezaguayo@gmail.com>
2025-05-21 08:26:52 +0000

    perfil

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,12 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+           <h1>${!variables.name == !false ? variables.name : "your name"} ${
+    variables.lastName
+  }</h1>
+          <h2>${variables.role || "Role"}</h2>
+          <h3>${variables.city || "City"}, ${variables.country ||
+    "Country"}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
